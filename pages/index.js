0130cb2bc263b7a198daf81a1f611be64c3dd343
Jerry-29 <govindsingh0498@gmail.com>
2022-03-28 00:01:21 +0530

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { Join } from './api/Join';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-     hey
+      <Join/>
     </div>
   );
 }
